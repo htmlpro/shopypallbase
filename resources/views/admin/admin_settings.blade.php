c14cf717bf237c548b3ca1be026e53146836306d
mdshoaibpk <mdshoaibpk90@gmail.com>
2021-03-03 22:57:09 +0500

    changes

--- resources/views/admin/admin_settings.blade.php
+++ resources/views/admin/admin_settings.blade.php
@@ -1,210 +1,254 @@
 @extends('admin.layout')
 @section('content')
-
-<div class="content-wrapper">
-
-	<section class="content">
+<style type="text/css">
+	.p-zeero{
+		justify-content: flex-start !important;
+	}
+</style>
+<div class="content-wrapper container">
+	<!-- Content Header (Page header) -->
+	<section class="content-header">
+		<h1>Settings</h1>
+		<ol class="breadcrumb">
+			<li><a href="{{ URL::to('admin/dashboard/this_month') }}"><i class="fa fa-dashboard"></i>
+			{{ trans('labels.breadcrumb_dashboard') }}</a></li>
+			<li class="active"> {{ trans('labels.Setting') }}</li>
+		</ol>
+	</section>
+	<section class="content" style="max-width: 106rem; margin-left: 0;">
 		<div class="row">
-			<div class="col-md-1">
-
-			</div>
-			<div class="col-md-10 settings-box">
-				<h1 style="margin-top: 0">
-					<small>Settings</small>
-				</h1>
-				<div class="box box-danger border-top-none card">
-					<div class="row">
-						<a href="{{url('admin/setting')}}" class="col-md-4">
-							<div class="col-md-2 box-icon">
-								<i class="fa fa-gear" aria-hidden="true"></i>
-							</div>
-							<div class="col-md-10">
-								<h4>
+			<a class="col-sm-6 col-md-6 col-lg-4" href="{{url('admin/websettings')}}">
+				<div class="box box-danger border-top-none card transition total-price">
+					<!-- /.box-header -->
+					<div class="box-body padding-0 p-zeero">
+						<div style="margin-right: 10px">
+							<div class="icon1"><i class="fa fa-gear" aria-hidden="true"></i></div>
+						</div>
+						<div class="">
+							<h4>
+								<b>
 									General
-								</h4>
-								<p>
-									View and update your store details
-								</p>
-							</div>
-						</a>
-						<a href="{{url('admin/countries/display')}}" class="col-md-4">
-							<div class="col-md-2 box-icon">
-								<i class="fa fa-map-marker" aria-hidden="true"></i>
-							</div>
-							<div class="col-md-10">
-								<h4>
-									Locations
-								</h4>
-								<p>
-									Manage the places you stock inventory, fulfill orders, and sell products
-								</p>
-							</div>
-						</a>
-						<a href="" class="col-md-4">
-							<div class="col-md-2 box-icon">
-								<i class="fa fa-user" aria-hidden="true"></i>
-							</div>
-							<div class="col-md-10">
-								<h4>
-									Plan and permissions
-								</h4>
-								<p>
-									View plan information and manage what staff can see or do in your store
-								</p>
-							</div>
-						</a>
-						<a href="{{url('admin/paymentmethods/index')}}" class="col-md-4">
-							<div class="col-md-2 box-icon">
-								<i class="fa fa-credit-card-alt" aria-hidden="true"></i>
-							</div>
-							<div class="col-md-10">
-								<h4>
-									Payments
-								</h4>
-								<p>
-									Enable and manage your store's payment providers
-								</p>
-							</div>
-						</a>
-						<a href="{{url('admin/pushnotification')}}" class="col-md-4">
-							<div class="col-md-2 box-icon">
-								<i class="fa fa-bell" aria-hidden="true"></i>
-							</div>
-							<div class="col-md-10">
-								<h4>
-									Notifications
-								</h4>
-								<p>
-									Manage notifications sent to you and your customers
-								</p>
-							</div>
-						</a>
-						<a href="{{url('admin/languages/display')}}" class="col-md-4">
-							<div class="col-md-2 box-icon">
-								<i class="fa fa-language" aria-hidden="true"></i>
-							</div>
-							<div class="col-md-10">
-								<h4>
-									Store languages
-								</h4>
-								<p>
-									Manage the languages your customers can view on your store
-								</p>
-							</div>
-						</a>
-						<a href="" class="col-md-4">
-							<div class="col-md-2 box-icon">
-								<i class="fa fa-shopping-cart" aria-hidden="true"></i>
-							</div>
-							<div class="col-md-10">
-								<h4>
-									Checkout
-								</h4>
-								<p>
-									Customize your online checkout process
-								</p>
-							</div>
-						</a>
-						<a href="" class="col-md-4">
-							<div class="col-md-2 box-icon">
-								<i class="fa fa-gift" aria-hidden="true"></i>
-							</div>
-							<div class="col-md-10">
-								<h4>
-									Gift cards
-								</h4>
-								<p>
-									Enable Apple Wallet passes and set gift card expiry dates
-								</p>
-							</div>
-						</a>
-						<a href="" class="col-md-4">
-							<div class="col-md-2 box-icon">
-								<i class="fa fa-usd" aria-hidden="true"></i>
-							</div>
-							<div class="col-md-10">
-								<h4>
-									Billing
-								</h4>
-								<p>
-									Manage your billing information and view your invoices
-								</p>
-							</div>
-						</a>
-						<a href="{{url('admin/shippingmethods/display')}}" class="col-md-4">
-							<div class="col-md-2 box-icon">
-								<i class="fa fa-truck" aria-hidden="true"></i>
-							</div>
-							<div class="col-md-10">
-								<h4>
-									Shipping and delivery
-								</h4>
-								<p>
-									Manage how you ship orders to customers
-								</p>
-							</div>
-						</a>
-						<a href="" class="col-md-4">
-							<div class="col-md-2 box-icon">
-								<i class="fa fa-files-o" aria-hidden="true"></i>
-							</div>
-							<div class="col-md-10">
-								<h4>
-									Files
-								</h4>
-								<p>
-									Upload images, videos, and documents
-								</p>
-							</div>
-						</a>
-						<a href="" class="col-md-4">
-							<div class="col-md-2 box-icon">
-								<i class="fa fa-gavel" aria-hidden="true"></i>
-							</div>
-							<div class="col-md-10">
-								<h4>
-									Legal
-								</h4>
-								<p>
-									Manage your store's legal pages
-								</p>
-							</div>
-						</a>
-						<a href="{{url('admin/tax/taxclass/display')}}" class="col-md-4">
-							<div class="col-md-2 box-icon">
-								<i class="fa fa-percent" aria-hidden="true"></i>
-							</div>
-							<div class="col-md-10">
-								<h4>
-									Taxes
-								</h4>
-								<p>
-									Manage how your store charges taxes
-								</p>
-							</div>
-						</a>
-						<a href="" class="col-md-4">
-							<div class="col-md-2 box-icon">
-								<img style="width: 60%" src="http://127.0.0.1:8000/images/media/2021/01/icon-sales.png">
-							</div>
-							<div class="col-md-10">
-								<h4>
-									Sales channels
-								</h4>
-								<p>
-									Manage the channels you use to sell your products and services
-								</p>
-							</div>
-						</a>
-
+								</b>
+							</h4>
+							<h4>View and update your store details</h4>
+						</div>
+						<!-- /.users-list -->
 					</div>
+					<!-- /.box-body -->
 				</div>
-			</div>
+			</a>
 
-			<div class="col-md-1">
+			<a class="col-sm-6 col-md-6 col-lg-4" href="{{url('admin/countries/display')}}">
+				<div class="box box-danger border-top-none card transition total-price">
+					<!-- /.box-header -->
+					<div class="box-body padding-0 p-zeero">
+						<div style="margin-right: 10px">
+							<div class="icon2"><i class="fa fa-map-marker" aria-hidden="true"></i></div>
+						</div>
+						<div class="">
+							<h4>
+								<b>
+									Locations
+								</b>
+							</h4>
+							<h4>Manage places for inventory, orders, and sell products</h4>
+						</div>
+						<!-- /.users-list -->
+					</div>
+					<!-- /.box-body -->
+				</div>
+			</a>
 
-			</div>
+			<a class="col-sm-6 col-md-6 col-lg-4" href="{{url('admin/manageroles')}}">
+				<div class="box box-danger border-top-none card transition total-price">
+					<!-- /.box-header -->
+					<div class="box-body padding-0 p-zeero">
+						<div style="margin-right: 10px">
+							<div class="icon4"><i class="fa fa-user" aria-hidden="true"></i></div>
+						</div>
+						<div class="">
+							<h4>
+								<b>
+									Permissions
+								</b>
+							</h4>
+							<h4>Manage what staff can see or do in your store</h4>
+						</div>
+						<!-- /.users-list -->
+					</div>
+					<!-- /.box-body -->
+				</div>
+			</a>
+
+			<a class="col-sm-6 col-md-6 col-lg-4" href="{{url('admin/paymentmethods/index')}}">
+				<div class="box box-danger border-top-none card transition total-price">
+					<!-- /.box-header -->
+					<div class="box-body padding-0 p-zeero">
+						<div style="margin-right: 10px">
+							<div class="icon3"><i class="fa fa-credit-card-alt" aria-hidden="true"></i></div>
+						</div>
+						<div class="">
+							<h4>
+								<b>
+									Payments
+								</b>
+							</h4>
+							<h4>Enable and manage your store's payment providers</h4>
+						</div>
+						<!-- /.users-list -->
+					</div>
+					<!-- /.box-body -->
+				</div>
+			</a>
+			<a class="col-sm-6 col-md-6 col-lg-4" href="{{url('admin/pushnotification')}}">
+				<div class="box box-danger border-top-none card transition total-price">
+					<!-- /.box-header -->
+					<div class="box-body padding-0 p-zeero">
+						<div style="margin-right: 10px">
+							<div class="icon1"><i class="fa fa-bell" aria-hidden="true"></i></div>
+						</div>
+						<div class="">
+							<h4>
+								<b>
+									Notifications
+								</b>
+							</h4>
+							<h4>Manage notifications sent to you and your customers</h4>
+						</div>
+						<!-- /.users-list -->
+					</div>
+					<!-- /.box-body -->
+				</div>
+			</a>
+
+			<a class="col-sm-6 col-md-6 col-lg-4" href="{{url('admin/languages/display')}}">
+				<div class="box box-danger border-top-none card transition total-price">
+					<!-- /.box-header -->
+					<div class="box-body padding-0 p-zeero">
+						<div style="margin-right: 10px">
+							<div class="icon2"><i class="fa fa-language" aria-hidden="true"></i></div>
+						</div>
+						<div class="">
+							<h4>
+								<b>
+									Store languages
+								</b>
+							</h4>
+							<h4>Manage the languages your customers can view on your store</h4>
+						</div>
+						<!-- /.users-list -->
+					</div>
+					<!-- /.box-body -->
+				</div>
+			</a>
+
+			<a class="col-sm-6 col-md-6 col-lg-4" href="{{url('admin/shippingmethods/display')}}">
+				<div class="box box-danger border-top-none card transition total-price">
+					<!-- /.box-header -->
+					<div class="box-body padding-0 p-zeero">
+						<div style="margin-right: 10px">
+							<div class="icon4"><i class="fa fa-truck" aria-hidden="true"></i></div>
+						</div>
+						<div class="">
+							<h4>
+								<b>
+									Shipping and delivery
+								</b>
+							</h4>
+							<h4>Manage how you ship orders to customers</h4>
+						</div>
+						<!-- /.users-list -->
+					</div>
+					<!-- /.box-body -->
+				</div>
+			</a>
+
+			<a class="col-sm-6 col-md-6 col-lg-4" href="{{url('admin/media/add')}}">
+				<div class="box box-danger border-top-none card transition total-price">
+					<!-- /.box-header -->
+					<div class="box-body padding-0 p-zeero">
+						<div style="margin-right: 10px">
+							<div class="icon3"><i class="fa fa-files-o" aria-hidden="true"></i></div>
+						</div>
+						<div class="">
+							<h4>
+								<b>
+									Files
+								</b>
+							</h4>
+							<h4>Upload images, videos, and documents</h4>
+						</div>
+						<!-- /.users-list -->
+					</div>
+					<!-- /.box-body -->
+				</div>
+			</a>
+
+			<a class="col-sm-6 col-md-6 col-lg-4" href="{{url('admin/tax/taxclass/display')}}">
+				<div class="box box-danger border-top-none card transition total-price">
+					<!-- /.box-header -->
+					<div class="box-body padding-0 p-zeero">
+						<div style="margin-right: 10px">
+							<div class="icon1"><i class="fa fa-percent" aria-hidden="true"></i></div>
+						</div>
+						<div class="">
+							<h4>
+								<b>
+									Taxes
+								</b>
+							</h4>
+							<h4>Manage how your store charges taxes</h4>
+						</div>
+						<!-- /.users-list -->
+					</div>
+					<!-- /.box-body -->
+				</div>
+			</a>
+
+			<a class="col-sm-6 col-md-6 col-lg-4" href="{{url('admin/setting')}}">
+				<div class="box box-danger border-top-none card transition total-price">
+					<!-- /.box-header -->
+					<div class="box-body padding-0 p-zeero">
+						<div style="margin-right: 10px">
+							<div class="icon2"><i class="fa fa-gear" aria-hidden="true"></i></div>
+						</div>
+						<div class="">
+							<h4>
+								<b>
+									Domain Settings
+								</b>
+							</h4>
+							<h4>Manage the your website domain, email and address information.</h4>
+						</div>
+						<!-- /.users-list -->
+					</div>
+					<!-- /.box-body -->
+				</div>
+			</a>
+
+			<a class="col-sm-6 col-md-6 col-lg-4" href="{{url('admin/currencies/display')}}">
+				<div class="box box-danger border-top-none card transition total-price">
+					<!-- /.box-header -->
+					<div class="box-body padding-0 p-zeero">
+						<div style="margin-right: 10px">
+							<div class="icon4"><i class="fa fa-gift" aria-hidden="true"></i></div>
+						</div>
+						<div class="">
+							<h4>
+								<b>
+									Currency Settings
+								</b>
+							</h4>
+							<h4>Manage the currencies for your store.</h4>
+						</div>
+						<!-- /.users-list -->
+					</div>
+					<!-- /.box-body -->
+				</div>
+			</a>
+
 		</div>
+
 
 	</section>
 </div>
